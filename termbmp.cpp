a480af1c99b221cf07cc89d02d033a16eb880baa
Paul Caron <45680511+paul-caron@users.noreply.github.com>
2020-09-04 23:06:22 -0500

    Update termbmp.cpp

--- termbmp.cpp
+++ termbmp.cpp
@@ -7,7 +7,33 @@ Date: September2, 2020
 #include <cstdint>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <cmath>
 using namespace std;
+
+#pragma pack(push, 1)
+
+struct BMPFileHeader {
+     uint16_t magic_number{0};
+     uint32_t file_size{0};
+     uint16_t reserved1{0};
+     uint16_t reserved2{0};
+     uint32_t data_offset{0};
+};
+
+struct DIBHeader {
+     uint32_t dib_header_size{ 0 };
+     int32_t image_width{ 0 };
+     int32_t image_height{ 0 };
+     uint16_t color_planes{ 1 };
+     uint16_t bits_per_pixel{0};
+     uint32_t compression_type{ 0 };
+     uint32_t image_size{ 0 };
+     int32_t x_pixels_per_meter{ 0 };
+     int32_t y_pixels_per_meter{ 0 };
+     uint32_t palette_colors{0};
+     uint32_t important_colors{0};
+};
+#pragma pack(pop)
 
 int main(int argc, char ** argv){
     struct winsize w;
@@ -25,109 +51,119 @@ int main(int argc, char ** argv){
         cerr<<"\033[31mCould not open file.\033[0m \n";
         exit(1);
     }
-    //file info
-    uint16_t mn;
-    uint32_t fbs;
-    uint16_t res1;
-    uint16_t res2;
-    uint32_t offset;
-    //dib header
-    uint32_t dib_header_size;
-    int32_t image_width;
-    int32_t image_height;
-    uint16_t color_planes;
-    uint16_t bits_per_pixel;
-    uint32_t compression_model;
-    uint32_t image_size;
-    int32_t hor_resolution;
-    int32_t vert_resolution;
-    uint32_t palette_colors;
-    uint32_t important_colors;
-    //file info
-    myFile.read(reinterpret_cast<char *>(&mn),sizeof(mn));
-    if(mn!=19778){
-        cerr<<"\033[31mFile Magic Number is not 'BM'.\033[0m \n";
+
+    BMPFileHeader bfh;
+    myFile.read(reinterpret_cast<char*>(&bfh),sizeof(bfh));
+    cout<<"magic number: "<<bfh.magic_number<<endl;
+    cout<<"file size: "<<bfh.file_size<<endl;
+    cout<<"reserve 1: "<<bfh.reserved1<<endl;
+    cout<<"reserve 2: "<<bfh.reserved2<<endl;
+    cout<<"data offset: "<<bfh.data_offset<<endl;
+    if(bfh.magic_number!=19778){
+        cerr<<"\033[31mThe file type is not BMP.\033[0m \n";
         exit(1);
     }
-    myFile.read((char *)&fbs, 4);
-    myFile.read((char *)&res1, 2);
-    myFile.read((char *)&res2, 2);
-    myFile.read((char *)&offset, 4);
-    cout<<"\nFile Info:\n"
-        <<mn<<endl
-        <<fbs<<endl
-        <<res1<<endl
-        <<res2<<endl
-        <<offset<<endl;
-    //dib header
-    myFile.read((char*)&dib_header_size,4);
-    if(dib_header_size!=40){
-   //     cerr<<"\033[31mThis version of DIB header is not supported.\033[0m \n";
-   //     exit(1);
-    }
-    myFile.read(reinterpret_cast<char*>(&image_width),sizeof(image_width));
-    myFile.read(reinterpret_cast<char*>(&image_height),sizeof(image_height));
-    myFile.read(reinterpret_cast<char*>(&color_planes),sizeof(color_planes));
-    myFile.read(reinterpret_cast<char*>(&bits_per_pixel),sizeof(bits_per_pixel));
-    myFile.read(reinterpret_cast<char*>(&compression_model),sizeof(compression_model));
-    if(compression_model!=0){
-        cerr<<"\033[31mCompression not supported.\033[0m \n";
+
+    DIBHeader dh;
+    myFile.read(reinterpret_cast<char*>(&dh),sizeof(dh));
+    cout<<"dib header size: "<<dh.dib_header_size<<endl;
+    if(dh.dib_header_size<40){
+        cerr<<"\033[31mThis DiB header is not supported.\033[0m \n";
         exit(1);
     }
-    myFile.read(reinterpret_cast<char*>(&image_size),sizeof(image_size));
-    myFile.read(reinterpret_cast<char*>(&hor_resolution),sizeof(hor_resolution));
-    myFile.read(reinterpret_cast<char*>(&vert_resolution),sizeof(vert_resolution));
-    myFile.read(reinterpret_cast<char*>(&palette_colors),sizeof(palette_colors));
-    myFile.read(reinterpret_cast<char*>(&important_colors),sizeof(important_colors));
+    cout<<"image width: "<<dh.image_width<<endl;
+    cout<<"image height: "<<dh.image_height<<endl;
+    cout<<"color planes: "<<dh.color_planes<<endl;
+    cout<<"bits per pixel: "<<dh.bits_per_pixel<<endl;
+    cout<<"compression type: "<<dh.compression_type<<endl;
+    cout<<"image size: "<<dh.image_size<<endl;
+    cout<<"palette colors: "<<dh.palette_colors<<endl;
+    cout<<"important colors: "<<dh.important_colors<<endl;
 
-    cout<<"\nDIB header:\n"
-        <<"dib header size: "<<dib_header_size<<endl
-        <<"width: "<<image_width<<endl
-        <<"height: "<<image_height<<endl
-        <<"color planes: "<<color_planes<<endl
-        <<"bits_per_pixel: "<<bits_per_pixel<<endl
-        <<"compression: "<<compression_model<<endl
-        <<"image_size: "<<image_size<<endl
-        <<"hor resolution: "<<hor_resolution<<endl
-        <<"vert resolution: "<<vert_resolution<<endl
-        <<"palette colors: "<<palette_colors<<endl
-        <<"important colors: "<<important_colors<<endl;
 
-    int palette_offset = 14 + dib_header_size ;
-    if(compression_model == 3) palette_offset += 12;
-    
-    myFile.seekg(palette_offset,myFile.beg);
-    char * palette = (char*) malloc(palette_colors*4);
-    myFile.read(palette, palette_colors*4);
+    int palette_offset=14+dh.dib_header_size;
+    if(dh.compression_type==3) palette_offset+=12;
 
-    myFile.seekg(offset, myFile.beg);
-    cout<< endl;
-    char * image_data = (char*)malloc(bits_per_pixel/8*image_width*image_height);
-    myFile.read(image_data, bits_per_pixel/8*image_width*image_height);
+    char * palette = new char[4*dh.palette_colors];
+    myFile.seekg(palette_offset, myFile.beg);
+    myFile.read(palette, 4*dh.palette_colors);
 
-    int ratio = image_width/w.ws_col+1;
-
-    for(int i=image_height-1;i>=0;i-=ratio){
-    for(int j=0;j<image_width*bits_per_pixel/8;j+=ratio*bits_per_pixel/8){
-        int index = image_size/image_height*i+j;
-        if(image_size==0) index=j+i*image_width*bits_per_pixel/8;
-        char red = image_data[index+2];
-        char green = image_data[index+1];
-        char blue = image_data[index];
-        if(bits_per_pixel==24)
-        cout<<"\033[48;2;"<<int(red)<<";"<<int(green)<<";"<<int(blue)<<"m ";
-        else if(bits_per_pixel==8){
-        green = palette[4*blue+1];
-        red = palette[4*blue+2];
-        blue = palette[4*blue];;
-        cout<<"\033[48;2;"<<int(red)<<";"<<int(green)<<";"<<int(blue)<<"m ";
-
-       }
+    myFile.seekg(bfh.data_offset,myFile.beg);
+    int abs_height = abs(dh.image_height);
+    int abs_width = abs(dh.image_width);
+    if(dh.image_size!=0) abs_width = dh.image_size/dh.image_height%2+abs_width;
+    char * image_data;
+    if(dh.image_size==0){
+        image_data= new char [abs_height*abs_width*dh.bits_per_pixel/8];
+        myFile.read(image_data,abs_height*abs_width*dh.bits_per_pixel/8);
+    }else{
+        image_data = new char [dh.image_size];
+        myFile.read(image_data,dh.image_size);
     }
-    cout<<"\033[0m"<<endl;
+    int ratio = abs_width/w.ws_col+1;
+    int pixel_bytes = dh.bits_per_pixel/8;
+    if(dh.image_height>=0){
+        for(int i=abs_height-1;i>=0;i-=ratio){
+            for(int j=0;j<pixel_bytes*abs_width;j+=pixel_bytes*ratio){
+               int index = dh.image_size/dh.image_height*i+j;
+               if(dh.image_size==0) index=j+i*dh.image_width*pixel_bytes;
+               if(dh.bits_per_pixel>=24){
+                    uint8_t blue = image_data[index];
+                    uint8_t green = image_data[index+1];
+                    uint8_t red = image_data[index+2];
+                    cout<<"\033[48;2;"<<+red<<";"<<+green<<";"<<+blue<<"m ";
+                }
+                if(dh.bits_per_pixel==8){
+                 if(dh.palette_colors!=0){
+                    int color_index = image_data[index]*4;
+                    uint8_t blue = palette[color_index];
+                    uint8_t green = palette[color_index + 1];
+                    uint8_t red = palette[color_index + 2];
+                    cout<< "\033[48;2;"<<+red<<";"<<+green<<";"<<+blue<<"m ";
+                 }
+                 else{
+                    int color = image_data[index];
+                    uint8_t blue = color;
+                    uint8_t green = color;
+                    uint8_t red = color;
+                    cout<< "\033[48;2;"<<+red<<";"<<+green<<";"<<+blue<<"m ";
+                 }
+                }
+            }
+            cout<<"\033[0m\n";
+        }
     }
-    myFile.close();
-    cout<<"\033[0m\n";
+    else{
+        for(int i=0;i<abs_height;i+=ratio){
+            for(int j=0;j<pixel_bytes*abs_width;j+=pixel_bytes*ratio){
+               int index = dh.image_size/abs_height*i+j;
+               if(dh.image_size==0) index=j+i*dh.image_width*pixel_bytes;
+               if(dh.bits_per_pixel==24){
+                    uint8_t blue = image_data[index];
+                    uint8_t green = image_data[index+1];
+                    uint8_t red = image_data[index+2];
+                    cout<<"\033[48;2;"<<+red<<";"<<+green<<";"<<+blue<<"m ";
+                }
+                if(dh.bits_per_pixel==8){
+                 if(dh.palette_colors!=0){
+                    int color_index = image_data[index]*4;
+                    uint8_t blue = palette[color_index];
+                    uint8_t green = palette[color_index + 1];
+                    uint8_t red = palette[color_index + 2];
+                    cout<< "\033[48;2;"<<+red<<";"<<+green<<";"<<+blue<<"m ";
+                 }
+                 else{
+                    int color = image_data[index];
+                    uint8_t blue = color;
+                    uint8_t green = color;
+                    uint8_t red = color;
+                    cout<< "\033[48;2;"<<+red<<";"<<+green<<";"<<+blue<<"m ";
+                 }
+                }
+            }
+            cout<<"\033[0m\n";
+        }
+    }
     free(palette);
     free(image_data);
     return 0;
